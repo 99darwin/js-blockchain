39452c5f7001287ef89e7b74ab74faa897c1328d
Nick Saponaro <nicksapsales@gmail.com>
2018-07-10 23:39:21 -0700

    url

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,12 @@ jQuery(
     $('#register').on('click', () => {
         let userIp = [];
         $.get({
-            url: 'http://localhost:6489/ip'
+            url: '/ip'
         }).done(response => {
             userIp.push(response.ip);
             console.log(userIp);
             $.post(
-                'http://localhost:6489/register-and-broadcast-node',
+                '/register-and-broadcast-node',
                 {
                     'newNodeUrl': 'http://' + userIp[0].host
                 },
